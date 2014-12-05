2e31f0a50e52a3fc4e9ba377a118b5b195cff77e
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-12-05 16:15:18 +0100

    install script

--- install_dependencies/install_dependencies_macosx.sh
+++ install_dependencies/install_dependencies_macosx.sh
@@ -15,9 +15,29 @@ git
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # to reinstall, do:
 # rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
-echo "export PATH=/usr/local/bin:/usr/local/sbin:$HOME/.gem/ruby/1.8/bin:$PATH" >> ~/.bash_profile
-echo "export PYTHONPATH=/usr/local/lib/python2.7/site-packages:/usr/local/opt/vtk5/lib/python2.7/site-packages:$PYTHONPATH" >> ~/.bash_profile
-echo "export QT_API=pyqt" >> ~/.bash_profile
+
+# adding correct variables
+# (see http://stackoverflow.com/questions/26775145/adding-directory-to-a-path-variable-like-path-just-once-in-bash )
+var=$(echo $PATH | grep -o  '/usr/local/bin')
+if [ -n "$var" ] ; then
+	echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile
+fi
+var=$(echo $PATH | grep -o  '/usr/local/sbin')
+if [ -n "$var" ] ; then
+	echo "export PATH=/usr/local/sbin:$PATH" >> ~/.bash_profile
+fi
+var=$(echo $PYTHONPATH | grep -o  '/usr/local/lib/python2.7/site-packages')
+if [ -n "$var" ] ; then
+	echo "export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH" >> ~/.bash_profile
+fi
+var=$(echo $PYTHONPATH | grep -o  '/usr/local/opt/vtk5/lib/python2.7/site-packages')
+if [ -n "$var" ] ; then
+	echo "export PYTHONPATH=/usr/local/opt/vtk5/lib/python2.7/site-packages:$PYTHONPATH" >> ~/.bash_profile
+fi
+var=$(echo $QT_API | grep -o  'pyqt')
+if [ -n "$var" ] ; then
+	echo "export QT_API=pyqt" >> ~/.bash_profile
+fi
 source ~/.bash_profile
 # Make sure we’re using the latest Homebrew
 brew install git
